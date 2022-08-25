7d0b53f11f16fb78041cc4064ba2391c6405fefe
Arun <arun.jb25@hotmail.co.uk>
2022-08-25 14:15:20 +0100

    comiting complete game to highest folder

---
+++ hangman/hangman_visulisation.py
@@ -0,0 +1,44 @@
+incorrect_guess1 = r""" 
+
+
+|_________             
+"""
+incorrect_guess2 = r""" 
+
+|     
+|     
+|    
+|     
+|   
+|_________             
+"""
+incorrect_guess3 = r""" 
+_______
+|     |  
+|     
+|    
+|    
+|    
+|_________             
+"""
+incorrect_guess4 = r""" 
+_______
+|     |  
+|     O
+|    
+|     
+|    
+|_________             
+"""
+
+incorrect_guess5 = r""" 
+_______
+|     |  
+|     O
+|    /|\
+|     |
+|    / \
+|_________             
+"""
+
+lives_visualisation = [incorrect_guess5,incorrect_guess4,incorrect_guess3,incorrect_guess2,incorrect_guess1]
